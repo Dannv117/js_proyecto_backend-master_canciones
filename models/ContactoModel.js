2f23270c8d3989fdd10991711be133a186791edb
Danna Valencia <92700031+Dannv117@users.noreply.github.com>
2021-12-06 21:04:00 -0500

    Add files via upload

---
+++ models/ContactoModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+//Definir un objeto Schema 
+const Schema = mongoose.Schema
+
+//Definir la estructura de las entidades 
+//a registrar
+
+const cancionesSchema = new Schema(
+    {
+        nombre: {
+            type: String,
+            required: "nombre vacio"
+        },
+        cantante: {
+            type: String,
+            required: "cantante vacio"
+        },genero: {
+            type:String,
+            required: "genero vacio"
+        },
+
+    }
+)
+
+export default cancionesSchema
